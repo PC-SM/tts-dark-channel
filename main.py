17085bdce4f6c5ecb6d3de582da53a2441f80062
PC-SM <gamedevpc42@gmail.com>
2026-04-19 11:19:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -153,3 +153,8 @@ async def montar(req: VideoRequest):
 @app.get("/")
 def health():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
