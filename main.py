8db7fcb31ddb042d1a37fbd6c126df45179893c2
PC-SM <gamedevpc42@gmail.com>
2026-04-18 22:46:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,25 +14,20 @@ class TTSRequest(BaseModel):
     voz: str = "pt-BR-AntonioNeural"
 
 def converter_pausas(texto: str) -> str:
-    texto = re.sub(r'\[PAUSA_LONGA\]', '<break time="2s"/>', texto)
-    texto = re.sub(r'\[PAUSA\]', '<break time="1s"/>', texto)
-    texto = re.sub(r'\.\.\.', '<break time="800ms"/>', texto)
-    ssml = f"""<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
-        xml:lang="pt-BR">
-        <voice name="{'{}'}">{texto}</voice>
-    </speak>"""
-    return ssml
+    texto = re.sub(r'\[PAUSA_LONGA\]', '. . . . .', texto)
+    texto = re.sub(r'\[PAUSA\]', '. . .', texto)
+    return texto
 
 @app.post("/narrar")
 async def narrar(req: TTSRequest):
     with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as f:
         tmpfile = f.name
-    
-    ssml = converter_pausas(req.texto).format(req.voz)
-    
-    communicate = edge_tts.Communicate(ssml, req.voz)
+
+    texto_limpo = converter_pausas(req.texto)
+
+    communicate = edge_tts.Communicate(texto_limpo, req.voz)
     await communicate.save(tmpfile)
-    
+
     with open(tmpfile, "rb") as f:
         audio_b64 = base64.b64encode(f.read()).decode()
     os.unlink(tmpfile)
