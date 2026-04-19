f539c3debbb17a33b67c2c7c1daeefef652774c6
PC-SM <gamedevpc42@gmail.com>
2026-04-18 22:40:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import base64
 import tempfile
 import os
+import re
 
 app = FastAPI()
 
@@ -12,12 +13,26 @@ class TTSRequest(BaseModel):
     texto: str
     voz: str = "pt-BR-AntonioNeural"
 
+def converter_pausas(texto: str) -> str:
+    texto = re.sub(r'\[PAUSA_LONGA\]', '<break time="2s"/>', texto)
+    texto = re.sub(r'\[PAUSA\]', '<break time="1s"/>', texto)
+    texto = re.sub(r'\.\.\.', '<break time="800ms"/>', texto)
+    ssml = f"""<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
+        xml:lang="pt-BR">
+        <voice name="{'{}'}">{texto}</voice>
+    </speak>"""
+    return ssml
+
 @app.post("/narrar")
 async def narrar(req: TTSRequest):
     with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as f:
         tmpfile = f.name
-    communicate = edge_tts.Communicate(req.texto, req.voz)
+    
+    ssml = converter_pausas(req.texto).format(req.voz)
+    
+    communicate = edge_tts.Communicate(ssml, req.voz)
     await communicate.save(tmpfile)
+    
     with open(tmpfile, "rb") as f:
         audio_b64 = base64.b64encode(f.read()).decode()
     os.unlink(tmpfile)
